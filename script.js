f8a09258baab6f22c416d2ad9a6313d7618c1c98
Jaka Mursid <jaka.mursidm@gmail.com>
2025-02-12 10:21:09 +0700

    Create function showRecentMovies for carousel or latest movies content

--- script.js
+++ script.js
@@ -21,4 +21,43 @@ function showMovies(){
           }
     })
 }
+
+function showRecentMovies(content) {
+    const currentYear = new Date().getFullYear();
+    $.ajax({
+        url : 'http://www.omdbapi.com/?apikey=42c0770b',
+        method : 'GET',
+        data :  {
+            s : 'movie',
+            y : currentYear
+        },
+        success: function (result) {
+            let movies = result.Search;
+            if(result.Response == "True"){
+                if(content == "carousel"){
+                    $.each(movies, function (i,data) {
+                        $('#carousel-movie-poster').append(`
+                             <div class="carousel-item active">
+                            <img src="`+data.Poster +`" class="d-block w-100" alt="Movie Poster">
+                        </div>`);   
+                        } );
+                }
+                else{
+                    
+                }
+                console.log(result);   
+            }else{
+                $('#movie-container').html(`<div class="alert alert-danger" role="alert">
+                Movie not Found!
+                </div>`)
+
+            }
+        },
+        error: function(error) {
+            console.error('Request failed:', error);
+          }
+    })
+}
+showRecentMovies(content="carousel");
+showRecentMovies(content="latest");
 showMovies();

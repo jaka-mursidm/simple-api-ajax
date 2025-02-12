d6a29472d28c283dda92cc314abe818ac0e40518
Jaka Mursid <jaka.mursidm@gmail.com>
2025-02-12 10:41:42 +0700

    Delete script show data in carousel

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function showMovies(){
     })
 }
 
-function showRecentMovies(content) {
+function showRecentMovies() {
     const currentYear = new Date().getFullYear();
     $.ajax({
         url : 'http://www.omdbapi.com/?apikey=42c0770b',
@@ -33,15 +33,7 @@ function showRecentMovies(content) {
         success: function (result) {
             let movies = result.Search;
             if(result.Response == "True"){
-                if(content == "carousel"){
-            movies.slice(0, 5).forEach(function (data, i) {
-                        let activated = i === 0 ? "active" : ""; 
-                        $('#carousel-movie-poster').append(`
-                            <div class="carousel-item ` + activated+`">
-                           <img src="`+data.Poster +`" class="d-block w-100" alt="Movie Poster">
-                       </div>`);  
-                        } );
-                }
+                
             }else{
                 $('#movie-container').html(`<div class="alert alert-danger" role="alert">
                 Movie not Found!
@@ -54,6 +46,4 @@ function showRecentMovies(content) {
           }
     })
 }
-showRecentMovies(content="carousel");
-showRecentMovies(content="latest");
 showMovies();

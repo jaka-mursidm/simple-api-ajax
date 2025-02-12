d8f98deaf8661b61741bf26648ac6e0d19e18344
Jaka Mursid <jaka.mursidm@gmail.com>
2025-02-12 17:06:53 +0700

    Successful show and search movies data in search page

--- script.js
+++ script.js
@@ -1,4 +1,42 @@
 // You can use this public API
+function searchMovies(){
+    const valueInput = $('#search-input').val() || 'movie';
+    $.ajax({
+        url : 'http://www.omdbapi.com/?apikey=42c0770b',
+        method : 'GET',
+        data :  {
+            s : valueInput
+        },
+        success: function (result) {
+            let movies = result.Search;
+            if(result.Response == "True"){
+                $('#search-movie-container').html(' ');
+                let movies = result.Search;        
+                $.each(movies, function (i,data) {
+                $('#search-movie-container').append(`
+                    <div class="col-md-4 mb-3">
+                        <div class="card bg-dark text-white">
+                            <img style="height:300px; width:100%; object-fit:cover;" src="`+ data.Poster +`" class="card-img-top" alt="`+ data.Title+`">
+                            <div class="card-body">
+                                <h5 class="card-title">`+ data.Title+`</h5>
+                                <p class="card-text">`+data.Year+`</p>
+                                <a href="#" class="btn btn-secondary">See Detail</a>
+                            </div>
+                        </div>
+                    </div>`);   
+                } );
+            }else{
+                $('#search-movie-container').html(`<div class="alert alert-danger" role="alert">
+                Movie not Found!
+                </div>`)
+
+            }
+        },
+        error: function(error) {
+            console.error('Request failed:', error);
+        }
+    })
+}
 function showMovies(){
     $.ajax({
         url : 'http://www.omdbapi.com/?apikey=42c0770b',
@@ -7,9 +45,25 @@ function showMovies(){
             s : 'movie'
         },
         success: function (result) {
+            let movies = result.Search;
             if(result.Response == "True"){
+                $('#search-movie-container').html(' ');
+                let movies = result.Search;      
+                $.each(movies, function (i,data) {
+                $('#search-movie-container').append(`
+                    <div class="col-md-4 mb-3">
+                        <div class="card bg-dark text-white">
+                            <img style="height:300px; width:100%; object-fit:cover;" src="`+ data.Poster +`" class="card-img-top" alt="`+ data.Title+`">
+                            <div class="card-body">
+                                <h5 class="card-title">`+ data.Title+`</h5>
+                                <p class="card-text">`+data.Year+`</p>
+                                <a href="#" class="btn btn-secondary">See Detail</a>
+                            </div>
+                        </div>
+                    </div>`);   
+                } );
             }else{
-                $('#movie-container').html(`<div class="alert alert-danger" role="alert">
+                $('#search-movie-container').html(`<div class="alert alert-danger" role="alert">
                 Movie not Found!
                 </div>`)
 
@@ -17,7 +71,7 @@ function showMovies(){
         },
         error: function(error) {
             console.error('Request failed:', error);
-          }
+        }
     })
 }
 
@@ -60,5 +114,17 @@ function showRecentMovies() {
           }
     })
 }
-showRecentMovies();
-showMovies();
+
+$(document).ready(function() {
+    $('#btn-search').on('click',function () {
+        searchMovies();
+    });
+    $('#search-input').on('keydown', function(event) {
+        if (event.which === 13 || event.keyCode === 13) { 
+            event.preventDefault(); // Mencegah submit default
+            searchMovies();
+        }
+    });
+    showRecentMovies();
+    showMovies();
+});

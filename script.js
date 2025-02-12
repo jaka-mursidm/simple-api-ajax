e3762c83963f32463dd801484bc74ba4185abe3e
Jaka Mursid <jaka.mursidm@gmail.com>
2025-02-12 12:57:43 +0700

    Show latest movies data to the latest movies container

--- script.js
+++ script.js
@@ -33,7 +33,21 @@ function showRecentMovies() {
         success: function (result) {
             let movies = result.Search;
             if(result.Response == "True"){
-                
+                let movies = result.Search;                
+                $.each(movies, function (i,data) {
+                $('#latest-movie-container').append(`
+                    <div class="col-md-4 mb-3">
+                        <div class="card">
+                            <img style="height:300px; width:100%; object-fit:cover;" src="`+ data.Poster +`" class="card-img-top" alt="`+ data.Title+`">
+                            <div class="card-body">
+                                <h5 class="card-title">`+ data.Title+`</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                                    the card's content.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>`);   
+                } );
             }else{
                 $('#movie-container').html(`<div class="alert alert-danger" role="alert">
                 Movie not Found!
@@ -46,4 +60,5 @@ function showRecentMovies() {
           }
     })
 }
+showRecentMovies();
 showMovies();
